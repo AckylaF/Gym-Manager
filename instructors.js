ae09d92cee8e32773595bceda86216d1a3bc7289
Ackyla Ferreira Batista <ackylafb@gmail.com>
2020-02-11 09:54:56 -0300

    a few adjustments

--- instructors.js
+++ instructors.js
@@ -1,11 +1,56 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+//show
+exports.show = function(req, res){
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        age: "",
+        services: foundInstructor.services.split(","),
+        created_at: ""
+    }
+
+    return res.render("instructors/show", {instructor})
+}
 // create
 exports.post = function(req, res){
-        const keys = Object.keys(req.body)
-    
-        for(key of keys){
-            if (req.body[key] == ""){
-                return res.send('Please, fill all fields')
-            }
+    const keys = Object.keys(req.body)
+
+    for(key of keys){
+        if (req.body[key] == ""){
+            return res.send('Please, fill all fields')
         }
-        return res.send(req.body)
+    }
+
+    let {avatar_url, name, birth, gender, services} = req.body
+
+    birth = Date.parse(req.body.birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push({
+        id,
+        name,
+        avatar_url,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("error")
+
+        return res.redirect("/instructors")
+    })
+
+   
 }
