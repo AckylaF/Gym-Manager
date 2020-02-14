82dc456cb4e1f47f5a22e24a90ab08201ecacf4c
Ackyla Ferreira Batista <ackylafb@gmail.com>
2020-02-14 11:14:06 -0300

    adjustments on editing instructors

--- instructors.js
+++ instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age, date } = require('./useful')
 
 //show
 exports.show = function(req, res){
@@ -12,9 +13,9 @@ exports.show = function(req, res){
 
     const instructor = {
         ...foundInstructor,
-        age: "",
+        age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: ""
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
     }
 
     return res.render("instructors/show", {instructor})
@@ -53,4 +54,22 @@ exports.post = function(req, res){
     })
 
    
+}
+//edit
+exports.edit = function(req, res){
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render("instructors/edit", { instructor })
+
 }
