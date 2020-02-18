16859cce0f15168cf62f2482d1e636f26c734ccc
Ackyla Ferreira Batista <ackylafb@gmail.com>
2020-02-18 09:28:45 -0300

    working on members page

--- instructors.js
+++ instructors.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./useful')
 
+exports.index = function(req, res){
+    return res.render("instructors/index", { instructors: data.instructors })
+}
 //show
 exports.show = function(req, res){
     const { id } = req.params
@@ -72,4 +75,48 @@ exports.edit = function(req, res){
 
     return res.render("instructors/edit", { instructor })
 
+}
+//put
+exports.put = function(req, res){
+    const { id } = req.body
+
+    let index = 0
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if (id == instructor.id){
+            index = foundIndex
+            return true
+        }
+    })
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+//delete
+exports.delete = function(req, res){
+    const { id } = req.body 
+
+    const filteredIntructors = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+
+    data.instructors = filteredIntructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) res.send("Write file error")
+
+        return res.redirect("/instructors")
+    })
 }
